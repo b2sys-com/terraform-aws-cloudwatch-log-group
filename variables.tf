4c63904a3cf987526a61e53fd54b8bbb9a6d04f3
diegosorace <dsorace@prismamp.com>
2023-02-16 10:57:03 -0300

    Update

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 variable "name" {
   description = "Name of logs and supporting resources."
   type        = string
-  #  validation {
-  #    error_message = "Can include numbers, lowercase letters, uppercase letters, and hyphens (-). It cannot start or end with a hyphen (-)."
-  #    condition     = can(regex("^[0-9a-zA-Z]+([0-9a-zA-Z-]*[0-9a-zA-Z])*$", var.name))
-  #  }
 }
 
 variable "retention_in_days" {
@@ -18,17 +14,3 @@ variable "tags" {
   type        = map(string)
   default     = null
 }
-
-#variable "kms_key_id" {
-#  description = "Encryption for KMS key."
-#  type        = string
-#}
-
-#variable "aws_service_principal" {
-#  description = "The name of the service being logged. 'Service Principal' is a special AWS identity that represents a service or other component with an easily understood name. You can find a list of AWS Service Principals at https://gist.github.com/shortjared/4c1e3fe52bdfa47522cfe5b41e5d6f22"
-#  type        = string
-#  validation {
-#    error_message = "The aws service to be logged cannot end or begin with a hyphen and must contain the suffix 'amazonaws.com'."
-#    condition     = can(regex("^[0-9a-z]+([0-9a-z-.]*[0-9a-z])*.amazonaws.com$", var.aws_service_principal))
-##  }
-#}
